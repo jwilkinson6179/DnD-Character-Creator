6bb74df7ccef6417a883a4b3f4b5fcf407babfa2
jwilkinson6179 <JamesW6179@gmail.com>
2020-08-12 16:41:41 -0400

    SpringInit

---
+++ src/main/java/com/wilkinson/DnDCharGen/DnDCharGenApplication.java
@@ -0,0 +1,13 @@
+package com.wilkinson.DnDCharGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDCharGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnDCharGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilkinson/DnDCharGen/DnDCharGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilkinson.DnDCharGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDCharGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
